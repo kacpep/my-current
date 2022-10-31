bcf76482986d04626c8821062b83e8a01245a702
Kacepr Ostrowski <kacper.ostrowski777@gmail.com>
2022-10-31 10:54:48 +0100

    add discord and hide twitter

--- app.js
+++ app.js
@@ -17,3 +17,17 @@ setInterval(() => {
 
 	mainTextLoading();
 }, 9000);
+
+let dc = document.querySelector("#copyDc");
+let dcText = document.querySelector("#copyDcText");
+let temp = dcText.textContent;
+
+dc.addEventListener("click", () => {
+	navigator.clipboard.writeText("kacpep#0828");
+	dcText.textContent = "✔ Copied";
+	setTimeout(() => {
+		dcText.style.opacity = "0%";
+		dcText.textContent = temp;
+		dcText.style.opacity = "100%";
+	}, 1000);
+});
